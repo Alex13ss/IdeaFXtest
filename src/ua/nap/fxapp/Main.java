da429cac55f490ef1858735de1fb36e313aaaec7
obandurka <obandur@softserveinc.com>
2015-12-01 16:17:13 +0200

    Grid Pane (Log In window)

--- src/ua/nap/fxapp/Main.java
+++ src/ua/nap/fxapp/Main.java
@@ -1,15 +1,17 @@
 package ua.nap.fxapp;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     Stage window;
-    Button button;
 
     public static void main(String[] args) {
         launch(args);
@@ -19,25 +21,32 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
         window.setTitle("Title of window");
-        window.setOnCloseRequest(event -> {
-            event.consume();
-            closeProgram();
-        });
-        button = new Button("Close Program");
-        button.setOnAction(event -> closeProgram());
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-        Scene scene = new Scene(layout, 300, 250);
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(8);
+        grid.setHgap(10);
+
+        Label nameLabel = new Label("Username:");
+        GridPane.setConstraints(nameLabel, 0, 0);
+
+        TextField nameInput = new TextField("Bucky");
+        GridPane.setConstraints(nameInput, 1, 0);
+
+        Label passLabel = new Label("Password:");
+        GridPane.setConstraints(passLabel, 0, 1);
+
+        TextField passInput = new TextField();
+        passInput.setPromptText("password");
+        GridPane.setConstraints(passInput, 1, 1);
+
+        Button loginButton = new Button("Log In");
+        GridPane.setConstraints(loginButton, 1, 2);
+
+        grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton);
+        Scene scene = new Scene(grid, 300, 200);
         window.setScene(scene);
         window.show();
-    }
-
-    private void closeProgram() {
-        Boolean answer = ConfirmBox.display("Stupid title", "Sure U want to EXIT?");
-        if (answer) {
-            window.close();
-        }
     }
 
 }
