4b816d70574f94fd26af8f60451efa1770f1a274
obandurka <obandur@softserveinc.com>
2015-12-02 13:20:36 +0200

    ComboBox

--- src/ua/nap/fxapp/Main.java
+++ src/ua/nap/fxapp/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,6 +13,7 @@ public class Main extends Application {
     Stage window;
     Scene scene;
     Button button;
+    ComboBox<String> comboBox;
 
     public static void main(String[] args) {
         launch(args);
@@ -24,18 +25,18 @@ public class Main extends Application {
         window.setTitle("Title of window");
         button = new Button("Click Me");
 
-        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        comboBox = new ComboBox<>();
+        comboBox.getItems().addAll("Good Will Hunting", "St. Vincent", "Blackhat");
+        comboBox.setPromptText("What is your favorite movie?");
 
-        choiceBox.getItems().add("Apples");
-        choiceBox.getItems().add("Bananas");
-        choiceBox.getItems().addAll("Bacon", "Ham", "Meatballs");
-        choiceBox.setValue("Apples");
+        comboBox.setEditable(true);
 
-        choiceBox.getSelectionModel().selectedItemProperty().addListener((v, oldValue, newValue) -> System.out.println(newValue));
+        button.setOnAction(event -> System.out.println(comboBox.getValue()));
+        comboBox.setOnAction(event -> System.out.println("User selected:" + comboBox.getValue()));
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(choiceBox, button);
+        layout.getChildren().addAll(comboBox, button);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
