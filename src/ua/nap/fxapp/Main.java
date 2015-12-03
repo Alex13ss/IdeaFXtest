8b03073093cdde38acea16ae35d403ed3d26c9e8
obandurka <obandur@softserveinc.com>
2015-12-03 18:58:43 +0200

    Expanded Menu + css

--- src/ua/nap/fxapp/Main.java
+++ src/ua/nap/fxapp/Main.java
@@ -27,17 +27,64 @@ public class Main extends Application {
         window = primaryStage;
         window.setTitle("JavaFX");
 
-        Menu fileMenu = new Menu("File");
+        // File Menu
+        Menu fileMenu = new Menu("_File");
 
-        fileMenu.getItems().add(new MenuItem("New Project..."));
-        fileMenu.getItems().add(new MenuItem("New Module..."));
-        fileMenu.getItems().add(new MenuItem("Import Project"));
+        MenuItem newFile = new MenuItem("New...");
+        newFile.setOnAction(event1 -> System.out.println("Create a new File..."));
+        fileMenu.getItems().add(newFile);
 
+        fileMenu.getItems().add(new MenuItem("Open..."));
+        fileMenu.getItems().add(new MenuItem("Save..."));
+        fileMenu.getItems().add(new SeparatorMenuItem());
+        fileMenu.getItems().add(new MenuItem("Settings..."));
+        fileMenu.getItems().add(new SeparatorMenuItem());
+        fileMenu.getItems().add(new MenuItem("Exit..."));
 
+        // Edit Menu
+        Menu editMenu = new Menu("_Edit");
+
+        editMenu.getItems().add(new MenuItem("Cut"));
+        editMenu.getItems().add(new MenuItem("Copy"));
+
+        MenuItem paste = new MenuItem("Paste");
+        paste.setOnAction(event1 -> System.out.println("Pasting some crap"));
+        paste.setDisable(true);
+        editMenu.getItems().add(paste);
+
+        // Help Menu
+        Menu helpMenu = new Menu("_Help");
+        CheckMenuItem showLines = new CheckMenuItem("Show line numbers");
+        showLines.setOnAction(event1 -> {
+            if (showLines.isSelected()) {
+                System.out.println("Program will now show line numbers");
+            } else {
+                System.out.println("Hiding line numbers");
+            }
+        });
+        CheckMenuItem autoSave = new CheckMenuItem("Enable Autosave");
+        autoSave.setSelected(true);
+        helpMenu.getItems().addAll(showLines, autoSave);
+
+        // RadioMenuItems
+        Menu difficultyMenu = new Menu("_Difficulty");
+        ToggleGroup difficultyToggle = new ToggleGroup();
+
+        RadioMenuItem easy = new RadioMenuItem("Easy");
+        RadioMenuItem medium = new RadioMenuItem("Medium");
+        RadioMenuItem hard = new RadioMenuItem("Hard");
+
+        easy.setToggleGroup(difficultyToggle);
+        medium.setToggleGroup(difficultyToggle);
+        hard.setToggleGroup(difficultyToggle);
+
+        difficultyMenu.getItems().addAll(easy, medium, hard);
+
+        // Menu Bar
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(fileMenu);
+        menuBar.getMenus().addAll(fileMenu, editMenu, difficultyMenu, helpMenu);
 
-
+        // Table Columns
         TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
         nameColumn.setMinWidth(200);
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -50,7 +97,7 @@ public class Main extends Application {
         quantityColumn.setMinWidth(100);
         quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
 
-
+        // Input Fields
         nameInput = new TextField();
         nameInput.setPromptText("Name");
         nameInput.setMinWidth(100);
@@ -61,7 +108,9 @@ public class Main extends Application {
         quantityInput = new TextField();
         quantityInput.setPromptText("Quantity");
 
+        // Buttons
         Button addButton = new Button("Add");
+        addButton.setId("bold-label");
         addButton.setOnAction(event -> addButtonClicked());
         Button deleteButton = new Button("Delete");
         deleteButton.setOnAction(event -> deleteButtonClicked());
@@ -71,16 +120,18 @@ public class Main extends Application {
         hBox.setSpacing(10);
         hBox.getChildren().addAll(nameInput, priceInput, quantityInput, addButton, deleteButton);
 
-
+        // Table
         table = new TableView<>();
         table.setItems(getProduct());
         table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
 
+        // Layout
         layout = new BorderPane();
         layout.setTop(menuBar);
         layout.setCenter(table);
         layout.setBottom(hBox);
         Scene scene = new Scene(layout, 600, 500);
+        scene.getStylesheets().setAll(getClass().getResource("AppMakeUp.css").toExternalForm());
         window.setScene(scene);
         window.show();
     }
