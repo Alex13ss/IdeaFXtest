401cfac751aa1f3ad6ad6bde93afb8b95cda111e
obandurka <obandur@softserveinc.com>
2015-12-04 15:17:31 +0200

    migration to FXML

--- src/ua/nap/fxapp/Controller.java
+++ src/ua/nap/fxapp/Controller.java
@@ -1,4 +1,84 @@
 package ua.nap.fxapp;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Initializing...");
+        table.setItems(getProduct());
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+    }
+
+    @FXML
+    private TextField priceInput;
+
+    @FXML
+    private TextField nameInput;
+
+    @FXML
+    private TextField quantityInput;
+
+    @FXML
+    private TableColumn<Product, String> nameColumn;
+
+    @FXML
+    private TableColumn<Product, Double> priceColumn;
+
+    @FXML
+    private TableColumn<Product, Integer> quantityColumn;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private TableView<Product> table;
+
+    @FXML
+    void addButtonClicked() {
+        Product product = new Product();
+        product.setName(nameInput.getText());
+        product.setPrice(Double.parseDouble(priceInput.getText()));
+        product.setQuantity(Integer.parseInt(quantityInput.getText()));
+        table.getItems().add(product);
+        nameInput.clear();
+        priceInput.clear();
+        quantityInput.clear();
+    }
+
+    @FXML
+    void deleteButtonClicked() {
+        ObservableList<Product> productSelected, allProducts;
+        allProducts = table.getItems();
+        productSelected = table.getSelectionModel().getSelectedItems();
+        productSelected.forEach(allProducts::remove);
+    }
+
+    public ObservableList<Product> getProduct() {
+        ObservableList<Product> products = FXCollections.observableArrayList();
+        products.add(new Product("Laptop", 799.99, 13));
+        products.add(new Product("Cell Phone", 599.99, 27));
+        products.add(new Product("Pop Corn", 5.00, 120));
+        products.add(new Product("Smth interesting", 999.95, 99));
+        products.add(new Product("Sweet Dreams", 12.50, 1000));
+        return products;
+    }
+
 }
--- src/ua/nap/fxapp/Main.java
+++ src/ua/nap/fxapp/Main.java
@@ -3,20 +3,24 @@ package ua.nap.fxapp;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class Main extends Application {
 
-    Stage window;
-    TableView<Product> table;
-    TextField nameInput, priceInput, quantityInput;
-    BorderPane layout;
+//    Stage window;
+//    TableView<Product> table;
+//    TextField nameInput, priceInput, quantityInput;
+//    BorderPane layout;
 
     public static void main(String[] args) {
         launch(args);
@@ -24,144 +28,157 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        window = primaryStage;
-        window.setTitle("JavaFX");
+//        window = primaryStage;
+//        window.setTitle("JavaFX");
+//
+//        // File Menu
+//        Menu fileMenu = new Menu("_File");
+//
+//        MenuItem newFile = new MenuItem("New...");
+//        newFile.setOnAction(event1 -> System.out.println("Create a new File..."));
+//        fileMenu.getItems().add(newFile);
+//
+//        fileMenu.getItems().add(new MenuItem("Open..."));
+//        fileMenu.getItems().add(new MenuItem("Save..."));
+//        fileMenu.getItems().add(new SeparatorMenuItem());
+//        fileMenu.getItems().add(new MenuItem("Settings..."));
+//        fileMenu.getItems().add(new SeparatorMenuItem());
+//        fileMenu.getItems().add(new MenuItem("Exit..."));
+//
+//        // Edit Menu
+//        Menu editMenu = new Menu("_Edit");
+//
+//        editMenu.getItems().add(new MenuItem("Cut"));
+//        editMenu.getItems().add(new MenuItem("Copy"));
+//
+//        MenuItem paste = new MenuItem("Paste");
+//        paste.setOnAction(event1 -> System.out.println("Pasting some crap"));
+//        paste.setDisable(true);
+//        editMenu.getItems().add(paste);
+//
+//        // Help Menu
+//        Menu helpMenu = new Menu("_Help");
+//        CheckMenuItem showLines = new CheckMenuItem("Show line numbers");
+//        showLines.setOnAction(event1 -> {
+//            if (showLines.isSelected()) {
+//                System.out.println("Program will now show line numbers");
+//            } else {
+//                System.out.println("Hiding line numbers");
+//            }
+//        });
+//        CheckMenuItem autoSave = new CheckMenuItem("Enable Autosave");
+//        autoSave.setSelected(true);
+//        helpMenu.getItems().addAll(showLines, autoSave);
+//
+//        // RadioMenuItems
+//        Menu difficultyMenu = new Menu("_Difficulty");
+//        ToggleGroup difficultyToggle = new ToggleGroup();
+//
+//        RadioMenuItem easy = new RadioMenuItem("Easy");
+//        RadioMenuItem medium = new RadioMenuItem("Medium");
+//        RadioMenuItem hard = new RadioMenuItem("Hard");
+//
+//        easy.setToggleGroup(difficultyToggle);
+//        medium.setToggleGroup(difficultyToggle);
+//        hard.setToggleGroup(difficultyToggle);
+//
+//        difficultyMenu.getItems().addAll(easy, medium, hard);
+//
+//        // Menu Bar
+//        MenuBar menuBar = new MenuBar();
+//        menuBar.getMenus().addAll(fileMenu, editMenu, difficultyMenu, helpMenu);
+//
+//        // Table Columns
+//        TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
+//        nameColumn.setMinWidth(200);
+//        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+//
+//        TableColumn<Product, Double> priceColumn = new TableColumn<>("Price");
+//        priceColumn.setMinWidth(100);
+//        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+//
+//        TableColumn<Product, Integer> quantityColumn = new TableColumn<>("Quantity");
+//        quantityColumn.setMinWidth(100);
+//        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+//
+//        // Input Fields
+//        nameInput = new TextField();
+//        nameInput.setPromptText("Name");
+//        nameInput.setMinWidth(100);
+//
+//        priceInput = new TextField();
+//        priceInput.setPromptText("Price");
+//
+//        quantityInput = new TextField();
+//        quantityInput.setPromptText("Quantity");
+//
+//        // Buttons
+//        Button addButton = new Button("Add");
+//        addButton.setId("bold-label");
+//        addButton.setOnAction(event -> addButtonClicked());
+//        Button deleteButton = new Button("Delete");
+//        deleteButton.setOnAction(event -> deleteButtonClicked());
+//
+//        HBox hBox = new HBox();
+//        hBox.setPadding(new Insets(10, 10, 10, 10));
+//        hBox.setSpacing(10);
+//        hBox.getChildren().addAll(nameInput, priceInput, quantityInput, addButton, deleteButton);
+//
+//        // Table
+//        table = new TableView<>();
+//        table.setItems(getProduct());
+//        table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
+//
+//        // Layout
+//        layout = new BorderPane();
+//        layout.setTop(menuBar);
+//        layout.setCenter(table);
+//        layout.setBottom(hBox);
+//        Scene scene = new Scene(layout, 600, 500);
+//        scene.getStylesheets().setAll(getClass().getResource("AppMakeUp.css").toExternalForm());
+//        window.setScene(scene);
+//        window.show();
 
-        // File Menu
-        Menu fileMenu = new Menu("_File");
 
-        MenuItem newFile = new MenuItem("New...");
-        newFile.setOnAction(event1 -> System.out.println("Create a new File..."));
-        fileMenu.getItems().add(newFile);
 
-        fileMenu.getItems().add(new MenuItem("Open..."));
-        fileMenu.getItems().add(new MenuItem("Save..."));
-        fileMenu.getItems().add(new SeparatorMenuItem());
-        fileMenu.getItems().add(new MenuItem("Settings..."));
-        fileMenu.getItems().add(new SeparatorMenuItem());
-        fileMenu.getItems().add(new MenuItem("Exit..."));
+        // constructing our scene
+        URL url = getClass().getResource("fxface.fxml");
+        AnchorPane pane = FXMLLoader.load(url);
+        Scene scene1 = new Scene(pane);
 
-        // Edit Menu
-        Menu editMenu = new Menu("_Edit");
-
-        editMenu.getItems().add(new MenuItem("Cut"));
-        editMenu.getItems().add(new MenuItem("Copy"));
-
-        MenuItem paste = new MenuItem("Paste");
-        paste.setOnAction(event1 -> System.out.println("Pasting some crap"));
-        paste.setDisable(true);
-        editMenu.getItems().add(paste);
-
-        // Help Menu
-        Menu helpMenu = new Menu("_Help");
-        CheckMenuItem showLines = new CheckMenuItem("Show line numbers");
-        showLines.setOnAction(event1 -> {
-            if (showLines.isSelected()) {
-                System.out.println("Program will now show line numbers");
-            } else {
-                System.out.println("Hiding line numbers");
-            }
-        });
-        CheckMenuItem autoSave = new CheckMenuItem("Enable Autosave");
-        autoSave.setSelected(true);
-        helpMenu.getItems().addAll(showLines, autoSave);
-
-        // RadioMenuItems
-        Menu difficultyMenu = new Menu("_Difficulty");
-        ToggleGroup difficultyToggle = new ToggleGroup();
-
-        RadioMenuItem easy = new RadioMenuItem("Easy");
-        RadioMenuItem medium = new RadioMenuItem("Medium");
-        RadioMenuItem hard = new RadioMenuItem("Hard");
-
-        easy.setToggleGroup(difficultyToggle);
-        medium.setToggleGroup(difficultyToggle);
-        hard.setToggleGroup(difficultyToggle);
-
-        difficultyMenu.getItems().addAll(easy, medium, hard);
-
-        // Menu Bar
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(fileMenu, editMenu, difficultyMenu, helpMenu);
-
-        // Table Columns
-        TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
-        nameColumn.setMinWidth(200);
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-
-        TableColumn<Product, Double> priceColumn = new TableColumn<>("Price");
-        priceColumn.setMinWidth(100);
-        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
-
-        TableColumn<Product, Integer> quantityColumn = new TableColumn<>("Quantity");
-        quantityColumn.setMinWidth(100);
-        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
-
-        // Input Fields
-        nameInput = new TextField();
-        nameInput.setPromptText("Name");
-        nameInput.setMinWidth(100);
-
-        priceInput = new TextField();
-        priceInput.setPromptText("Price");
-
-        quantityInput = new TextField();
-        quantityInput.setPromptText("Quantity");
-
-        // Buttons
-        Button addButton = new Button("Add");
-        addButton.setId("bold-label");
-        addButton.setOnAction(event -> addButtonClicked());
-        Button deleteButton = new Button("Delete");
-        deleteButton.setOnAction(event -> deleteButtonClicked());
-
-        HBox hBox = new HBox();
-        hBox.setPadding(new Insets(10, 10, 10, 10));
-        hBox.setSpacing(10);
-        hBox.getChildren().addAll(nameInput, priceInput, quantityInput, addButton, deleteButton);
-
-        // Table
-        table = new TableView<>();
-        table.setItems(getProduct());
-        table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
-
-        // Layout
-        layout = new BorderPane();
-        layout.setTop(menuBar);
-        layout.setCenter(table);
-        layout.setBottom(hBox);
-        Scene scene = new Scene(layout, 600, 500);
-        scene.getStylesheets().setAll(getClass().getResource("AppMakeUp.css").toExternalForm());
-        window.setScene(scene);
-        window.show();
+        // setting the stage
+        scene1.getStylesheets().setAll(getClass().getResource("AppMakeUp.css").toExternalForm());
+        primaryStage.setScene(scene1);
+        primaryStage.setTitle("Test FX");
+        primaryStage.show();
     }
 
-    public void addButtonClicked() {
-        Product product = new Product();
-        product.setName(nameInput.getText());
-        product.setPrice(Double.parseDouble(priceInput.getText()));
-        product.setQuantity(Integer.parseInt(quantityInput.getText()));
-        table.getItems().add(product);
-        nameInput.clear();
-        priceInput.clear();
-        quantityInput.clear();
-    }
+//    public void addButtonClicked() {
+//        Product product = new Product();
+//        product.setName(nameInput.getText());
+//        product.setPrice(Double.parseDouble(priceInput.getText()));
+//        product.setQuantity(Integer.parseInt(quantityInput.getText()));
+//        table.getItems().add(product);
+//        nameInput.clear();
+//        priceInput.clear();
+//        quantityInput.clear();
+//    }
 
-    public void deleteButtonClicked() {
-        ObservableList<Product> productSelected, allProducts;
-        allProducts = table.getItems();
-        productSelected = table.getSelectionModel().getSelectedItems();
-        productSelected.forEach(allProducts::remove);
-    }
+//    public void deleteButtonClicked() {
+//        ObservableList<Product> productSelected, allProducts;
+//        allProducts = table.getItems();
+//        productSelected = table.getSelectionModel().getSelectedItems();
+//        productSelected.forEach(allProducts::remove);
+//    }
 
-    public ObservableList<Product> getProduct() {
-        ObservableList<Product> products = FXCollections.observableArrayList();
-        products.add(new Product("Laptop", 799.99, 13));
-        products.add(new Product("Cell Phone", 599.99, 27));
-        products.add(new Product("Pop Corn", 5.00, 120));
-        products.add(new Product("Smth interesting", 999.95, 99));
-        products.add(new Product("Sweet Dreams", 12.50, 1000));
-        return products;
-    }
+//    public ObservableList<Product> getProduct() {
+//        ObservableList<Product> products = FXCollections.observableArrayList();
+//        products.add(new Product("Laptop", 799.99, 13));
+//        products.add(new Product("Cell Phone", 599.99, 27));
+//        products.add(new Product("Pop Corn", 5.00, 120));
+//        products.add(new Product("Smth interesting", 999.95, 99));
+//        products.add(new Product("Sweet Dreams", 12.50, 1000));
+//        return products;
+//    }
 
 }
